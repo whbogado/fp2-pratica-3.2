9433da4ffea83a074ad4efd6c34de38100fb77c0
Wilson <Wilson@homeserver>
2014-05-09 20:50:45 -0300

    Versão inicial.

---
+++ src/main/java/Pratica32.java
@@ -0,0 +1,17 @@
+/**
+ * IF62C Fundamentos de Programação 2
+ * Exercício de programação em Java.
+ * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
+ */
+public class Pratica32 {
+    public static void main(String[] args) {
+        double d = densidade(-1, 67, 3);
+        System.out.println("A densidade no ponto é " + d);
+    }
+    
+    public static double densidade(double x, double media, double desvio) {
+        double d = 1.0 / (Math.sqrt(2 * Math.PI) * desvio) *
+                    Math.exp(-0.5 * Math.pow((x - media) / desvio, 2));
+        return d;
+    }
+}
